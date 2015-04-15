b1029916f6d80a09285d02eb523ef37596aacac5
Nicholas Peters <nickpeters731@gmail.com>
2015-04-14 21:55:42 -0400

    first commit

--- CoreData/vcMain.swift
+++ CoreData/vcMain.swift
@@ -15,10 +15,10 @@ class vcMain: UIViewController {
     @IBOutlet var txtPassword: UITextField!
     
     @IBAction func btnSave() {
-        var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
         var context: NSManagedObjectContext = appDel.managedObjectContext!
         
-        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as NSManagedObject
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as! NSManagedObject
         newUser.setValue("" + txtUsername.text, forKey: "username")
         newUser.setValue("" + txtPassword.text, forKey: "password")
         
@@ -29,19 +29,19 @@ class vcMain: UIViewController {
     }
     
     @IBAction func btnLoad() {
-        var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        var appDel: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
         var context: NSManagedObjectContext = appDel.managedObjectContext!
         
         var request = NSFetchRequest(entityName: "Users")
         request.returnsObjectsAsFaults = false
         request.predicate = NSPredicate(format: "username = %@", txtUsername.text)
         
-        var results: NSArray = context.executeFetchRequest(request, error: nil) as [NSManagedObject]!
+        var results: NSArray = context.executeFetchRequest(request, error: nil) as! [NSManagedObject]!
         
         if(results.count > 0) {
-            var res = results[0] as NSManagedObject
-            txtUsername.text = res.valueForKey("username") as String
-            txtPassword.text = res.valueForKey("password") as String
+            var res = results[0] as! NSManagedObject
+            txtUsername.text = res.valueForKey("username") as! String
+            txtPassword.text = res.valueForKey("password") as! String
         } else {
             println("0 Results Returned... Potential Error")
         }
